c591c15722f29a7ee33bf01e03cc88f389b4449f
Anant-Singh1 <97743499+Anant-Singh1@users.noreply.github.com>
2023-12-25 21:19:54 +0530

    put a background image

--- src/com/async/gaming/GameFrame.java
+++ src/com/async/gaming/GameFrame.java
@@ -3,13 +3,15 @@ package com.async.gaming;
 import javax.swing.*;
 
 public class GameFrame extends JFrame {
-
+GameBoard gameBoard;
     public GameFrame(){
+        gameBoard=new GameBoard();
         setSize(1500,950);
         setResizable(false);
         setVisible(true);
         setTitle("Run For It");
         setLocationRelativeTo(null);
+        add(gameBoard);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
     public static void main(String[] args) {
